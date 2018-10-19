938e17c06c27a06a448a69712e87dc7300b58716
abhilashduggi <duggiabhilash@gmail.com>
2018-10-18 19:25:39 -0500

    piglatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,7 +10,54 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
 
+  word = word.toLowerCase();
+  let userInputSplit = word.split("");
+  let newArrayStartingwithVowel = [];
+  let newArrayBeforeVowel = [];
+  if(checkifVowel(userInputSplit[0])) {
+    userInputSplit.push('y');
+    userInputSplit.push('a');
+    userInputSplit.push('y');
+    return console.log(userInputSplit.join(""));
+  }
+  else {
+    for(let i = 0 ; i < userInputSplit.length; i++) {
+      if(checkifVowel(userInputSplit[i])) {
+        newArrayStartingwithVowel = userInputSplit.slice(i,(userInputSplit.length+1));
+        break;
+      }
+      else {
+        newArrayBeforeVowel.push(userInputSplit[i]);
+      }
+    }
+    if(newArrayStartingwithVowel.length == 0)
+    return console.log(word + " is not having any vowel");
+
+    let finalarray = newArrayStartingwithVowel.concat(newArrayBeforeVowel);
+    finalarray.push('a');
+    finalarray.push('y');
+    return console.log(finalarray.join(""));
+  }
+  
+  //return console.log(userInputSplit);
   // Your code here
+/*
+User inputs a text.
+Validate the input to see if the word has only letters- validateInput(userinput)
+if not a valid input prompt user for a valid input.
+checkifVowel() function compares the letter with a,e,i,o,u to see if the letter is vowel
+if userinput is valid, check for the first letter in the word - checkFirstLetter(userinput)
+  if first letter is a vowel  return true.
+if checkFirstLetter returns true push 'yay' to the user input string at the end and return it.
+else
+in doPigLatin where we split the userinput using .split("") and loop through the array to find a vowel
+join all the letters in a newarray newArrayBeforeVowel as we loop till we find the vowel and store in consonant.
+if vowel found slice the array starting with vowel to the end of the array and store it in a new
+array newArrayStartingwithVowel and break the for loop
+if newArrayStartingwithVowel lenght is 0 that means we did not have any vowels in the userinput string.
+concat newArrayStartingwithVowel with newArrayBeforeVowel
+push letters a and y to pigLating array and join the array and return the string.
+*/
 
 }
 
@@ -22,7 +69,15 @@ function getPrompt() {
   });
 }
 
+const checkifVowel =(letter) => {
+  if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+  return true;
+  else 
+  return false;
+}
+
 // Tests
+
 
 if (typeof describe === 'function') {
 
